31f1594d8a30998432b7afc4943047a521be29f5
codmccabe <codmccabe@users.noreply.github.com>
2025-10-22 12:53:55 +0000

    deploy: 0c80b71bd026ebfae358cad62950dac282d93065

--- Math1431/lunr-pretext-search-index.js
+++ Math1431/lunr-pretext-search-index.js
@@ -1,13 +1,40 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "[preface]",
+  "number": "",
+  "title": "[preface]",
+  "body": " Welcome to Pre-Calculus I. This text covers fundamental concepts necessary for calculus.  "
+},
+{
   "id": "sec-section11",
   "level": "1",
   "url": "sec-section11.html",
-  "type": "Section",
+  "type": "[section]",
   "number": "1.1",
   "title": "Linear Equations",
   "body": " Linear Equations  Your content here...  You can use math:  "
+},
+{
+  "id": "solutions",
+  "level": "1",
+  "url": "solutions.html",
+  "type": "[appendix]",
+  "number": "A",
+  "title": "Selected Solutions",
+  "body": " Selected Solutions  "
+},
+{
+  "id": "references",
+  "level": "1",
+  "url": "references.html",
+  "type": "[references]",
+  "number": "",
+  "title": "References",
+  "body": " References     "
 }
 ]
 
